0aef99bc88dbef9fd7a57dea5e6845dd8885c707
Dmitriy Gurov <DGurov81@gmail.com>
2019-11-03 18:39:15 +0300

    one to many

--- src/main/java/com/example/sweater/controller/RegistrationController.java
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -25,7 +25,7 @@ public class RegistrationController {
         User userFromDb = userRepo.findByUsername(user.getUsername());
 
         if(userFromDb != null){
-            model.put("message", "User Exist!");
+            model.put("message", "User exists!");
             return "registration";
         }
 
